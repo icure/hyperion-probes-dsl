45a8a8e03c96045bc0de4bb8c660d10e6a15776c
Vincenzo Pierro <vcp@icure.com>
2023-08-01 17:07:07 +0200

    Added cloudbuild files

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,13 +2,30 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 val ktorVersion = "2.2.3"
 
+buildscript {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+        maven { url = uri("https://maven.taktik.be/content/groups/public") }
+        maven { url = uri("https://repo.spring.io/plugins-release") }
+    }
+    dependencies {
+        classpath("com.taktik.gradle:gradle-plugin-git-version:2.0.4")
+        classpath("com.taktik.gradle:gradle-plugin-helm-repository:0.2.21-99208035f3")
+    }
+}
+
 plugins {
     kotlin("jvm") version "1.7.22"
     kotlin("plugin.serialization") version "1.7.20"
 }
 
+apply(plugin = "git-version")
+
 group = "com.icure"
-version = "1.0-SNAPSHOT"
+
+val gitVersion: String? by project
+version = gitVersion ?: "0.0.1-SNAPSHOT"
 
 repositories {
     mavenCentral()
