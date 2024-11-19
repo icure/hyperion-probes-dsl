b89ee5a9ade02822a4206e48bb6fe055a0ba4e33
Vincenzo Pierro <vcp@icure.com>
2024-11-19 12:37:59 +0100

    Fixed TimeWindowCollector test

--- src/test/kotlin/com/icure/monitoring/probes/dsl/collectors/TimeWindowCollectorTest.kt
+++ src/test/kotlin/com/icure/monitoring/probes/dsl/collectors/TimeWindowCollectorTest.kt
@@ -28,16 +28,21 @@ class TimeWindowCollectorTest : StringSpec({
 
     "A TimeWindowCollector will aggregate the samples with the specified granularity" {
         val clock = FakeClock()
-        val collector = TimeWindowCollector(Duration.ofMinutes(1), Duration.ofSeconds(1), clock)
+        val timeFrame = Duration.ofMinutes(1)
+        val collector = TimeWindowCollector(timeFrame, Duration.ofSeconds(1), clock)
 
+        // I advance the clock of one timeframe,
+        // otherwise the collector will return null because less than a timeframe passed
+        clock.advance(timeFrame.toMillis())
         val size = 10
-        val samples = 1000
+        val samples = 100
         (0 until size).forEach { value ->
             clock.advance(1_000)
             (0 until samples).forEach { _ ->
                 collector.addValue(value.toDouble())
             }
         }
+
 
         collector.getValues() shouldBe (0 until size).flatMap { v -> (0 until samples).map { v.toDouble() } }
         collector.max() shouldBe (size - 1).toDouble()
