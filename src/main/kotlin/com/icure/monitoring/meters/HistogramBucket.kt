46099b834233d78b21f52716e29b3670d3d566b1
Vincenzo Pierro <vcp@icure.com>
2024-11-26 10:56:44 +0100

    Switched back to async cache + immutable histogram

--- src/main/kotlin/com/icure/monitoring/meters/HistogramBucket.kt
+++ src/main/kotlin/com/icure/monitoring/meters/HistogramBucket.kt
@@ -9,30 +9,32 @@ import java.io.PrintStream
 import kotlin.math.max
 
 data class HistogramBucket(
-    val histogram: Histogram,
-    private var sumAccumulator: Double = 0.0,
-    private var maxAccumulator: Double = 0.0
+	val histogram: Histogram,
+	private var sumAccumulator: Double = 0.0,
+	private var maxAccumulator: Double = 0.0
 ) {
-    companion object {
-        fun create(layout: Layout) = HistogramBucket(Histogram.createDynamic(layout))
-    }
-    val sum: Double get() = sumAccumulator
-    val max: Double get() = maxAccumulator
+	companion object {
+		fun create(layout: Layout) = HistogramBucket(Histogram.createDynamic(layout))
+	}
+	val sum: Double get() = sumAccumulator
+	val max: Double get() = maxAccumulator
 
-    fun addValue(amount: Double) {
-        histogram.addValue(amount)
-        sumAccumulator += amount
-        maxAccumulator = max(maxAccumulator, amount)
-    }
+	fun addValue(amount: Double) {
+		histogram.addValue(amount)
+		sumAccumulator += amount
+		maxAccumulator = max(maxAccumulator, amount)
+	}
 
-    fun toHistogramSnapshot(percentiles: Iterable<Double>? = null, summaryOutput: ((PrintStream, Double) -> Unit)? = null): HistogramSnapshot =
-        HistogramSnapshot(
-            histogram.totalCount,
-            sum,
-            max,
-            percentiles?.map { ValueAtPercentile(it, histogram.getQuantile(it)) }?.toTypedArray(),
-            histogram.nonEmptyBinsAscending().map { CountAtBucket(it.binIndex.toDouble(), it.binCount.toDouble()) }.toTypedArray(),
-            summaryOutput
-        )
+	fun toHistogramSnapshot(percentiles: Iterable<Double>? = null, summaryOutput: ((PrintStream, Double) -> Unit)? = null): HistogramSnapshot =
+		histogram.preprocessedCopy.let { immutableHistogram ->
+			HistogramSnapshot(
+				immutableHistogram.totalCount,
+				sum,
+				max,
+				percentiles?.map { ValueAtPercentile(it, immutableHistogram.getQuantile(it)) }?.toTypedArray(),
+				immutableHistogram.nonEmptyBinsAscending().map { CountAtBucket(it.binIndex.toDouble(), it.binCount.toDouble()) }.toTypedArray(),
+				summaryOutput
+			)
+		}
 
 }
