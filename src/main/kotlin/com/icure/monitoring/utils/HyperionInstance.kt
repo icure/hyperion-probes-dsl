eae9db6dd9d4a823ea0fc8ae33fe389d4f95ad1c
Vincenzo Pierro <vcp@icure.com>
2025-10-01 11:14:33 +0200

    Added logival_gw target

--- src/main/kotlin/com/icure/monitoring/utils/HyperionInstance.kt
+++ src/main/kotlin/com/icure/monitoring/utils/HyperionInstance.kt
@@ -1,3 +1,13 @@
 package com.icure.monitoring.utils
 
-enum class HyperionInstance { LOCAL, DEV, PROD, OVH, FHC, GSC_PRD, GSC_ACC, BU }
+enum class HyperionInstance {
+	LOCAL,
+	DEV,
+	PROD,
+	OVH,
+	FHC,
+	GSC_PRD,
+	GSC_ACC,
+	BU,
+	LOGIVAL_GW
+}
